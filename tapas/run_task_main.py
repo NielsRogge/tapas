31ccb8ce388e405da8a3679aacbd0f2d3a4b7cdd
Rogge Niels <niels.rogge@howest.be>
2020-08-28 18:22:06 +0200

    Bug fix

--- tapas/run_task_main.py
+++ tapas/run_task_main.py
@@ -579,7 +579,7 @@ def _predict_for_set(
     print(i)
     print(type(prediction))
     print(type(prediction['pooled_output']))
-    print(prediction["pooled_output"].shape)
+    print(prediction["pooled_output"])
   exp_prediction_utils.write_predictions(
       result,
       prediction_file,
