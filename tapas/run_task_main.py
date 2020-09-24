7e63d47af97a96d3c2be5feea6906f515926f413
Rogge Niels <niels.rogge@howest.be>
2020-09-24 13:25:58 +0200

    Added evaluation

--- tapas/run_task_main.py
+++ tapas/run_task_main.py
@@ -564,19 +564,20 @@ def _predict_for_set(
     num_eval_steps=20,
   )
   eval_input_fn = functools.partial(
-      name='evaluate',
-      file_patterns=example_file,
-      data_format='tfrecord',
-      compression_type=FLAGS.compression_type,
-      is_training=False,
-      max_seq_length=FLAGS.max_seq_length,
-      max_predictions_per_seq=_MAX_PREDICTIONS_PER_SEQ,
-      add_aggregation_function_id=do_model_aggregation,
-      add_classification_labels=False,
-      add_answer=use_answer_as_supervision,
-      include_id=False,
-      params=params
-
+    func=tapas_classifier_model.input_fn,
+    name='evaluate',
+    file_patterns=example_file,
+    data_format='tfrecord',
+    compression_type=FLAGS.compression_type,
+    is_training=False,
+    max_seq_length=FLAGS.max_seq_length,
+    max_predictions_per_seq=_MAX_PREDICTIONS_PER_SEQ,
+    add_aggregation_function_id=do_model_aggregation,
+    add_classification_labels=False,
+    add_answer=use_answer_as_supervision,
+    include_id=False,
+    params=params,
+  )
   )
   eval_metrics = estimator.evaluate(input_fn=eval_input_fn, steps=params["num_eval_steps"])
   # end of experiment
