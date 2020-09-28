84ea0a75bee61ed3f9c54d48d7b3225c1295e63c
Rogge Niels <niels.rogge@howest.be>
2020-09-28 09:47:02 +0200

    Bug fix with print statement

--- tapas/models/tapas_classifier_model.py
+++ tapas/models/tapas_classifier_model.py
@@ -914,13 +914,13 @@ def _get_classification_outputs(
     )
 
     logits = tf.Print(logits,
-                      [_get_probs(dist_per_token) * input_mask_float][0],
+                      [_get_probs(dist_per_token) * input_mask_float[0]],
                       "Probs per token for first example in batch",
                       summarize=-1
     )
 
     logits = tf.Print(logits,
-                      [tf.reduce_sum(_get_probs(dist_per_token) * input_mask_float)[0], axis=0)],
+                      [tf.reduce_sum(_get_probs(dist_per_token) * input_mask_float[0], axis=0)],
                       "Number of probs equal one for first example in batch",
                       summarize=-1
     )
