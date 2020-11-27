9488d3ef50a718d335ba67793b3d89de8068b6f5
Rogge Niels <niels.rogge@howest.be>
2020-11-27 18:42:20 +0100

    Set max_steps to 50008

--- tapas/run_task_main.py
+++ tapas/run_task_main.py
@@ -467,7 +467,7 @@ def _train_and_predict(
     estimator.train(
         input_fn=train_input_fn,
         #max_steps=tapas_config.num_train_steps,
-        max_steps=None,
+        max_steps=50008,
     )
 
   elif mode == Mode.PREDICT_AND_EVALUATE or mode == Mode.PREDICT:
